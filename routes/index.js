f5abd5fca7050c041a490524f504b52ccae5c129
aixion <aixion1506@gmail.com>
2024-04-03 16:24:44 +0900

    회원가입 라우트 추가

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,43 @@
 const { Router } = require('express');
+const asyncHandler = require('../utils/async-handler');
+const hashPassword = require('../utils/hash-password');
+const { User } = require('../models');
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.redirect('/posts');
+  if (req.user) {
+    res.redirect('/posts');
+    return;
+  }
+  res.redirect('/login');
+  
 });
+
+router.get('/login', (req, res, next) => {
+  res.render('user/login');
+});
+
+router.get('/join', (req, res, next) => {
+  res.render('user/join');
+});
+
+router.get('/', (req, res, nex) => {
+  res.render('user/join');
+});
+
+router.post('join', asyncHandler(async(req, res) => {
+  const {email, name, password} = req.body;
+  const hashedPassword = hashPassword(password);
+  const user = await User.create({
+    email,
+    name,
+    password: hashedPassword,
+  });
+
+  console.log('신규회원', user);
+
+  res.redirect('/');
+  }),
+);
 
 module.exports = router;
